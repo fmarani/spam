57ed45768451edd979d98de7f9994b54cc35ef47
Federico Marani <federico.marani@tangentlabs.co.uk>
2011-04-13 13:25:24 +0100

    setup.py to use setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='spam',
+        version='0.1.0',
+        url='https://github.com/fmarani/spam',
+        description="Spam related services interface",
+        author="Federico Marani",
+        author_email="flagzeta@gmail.com",
+        package_dir={'': '.'},
+        requires=[],    
+        classifiers = [
+            "Intended Audience :: Developers",
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries',
+            ]
+        )
