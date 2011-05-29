f82a8257ee5a6b2b468eff1e32fabe683d3552af
Federico Marani <flagzeta@gmail.com>
2011-05-29 12:14:27 +0100

    bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='spam',
-        version='0.1.0',
+        version='0.2.0',
         url='https://github.com/fmarani/spam',
         description="Spam related services interface",
         author="Federico Marani",
