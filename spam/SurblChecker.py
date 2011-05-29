3a904e231f2deb781be1a8f33e7aeaa668b521a3
Federico Marani <flagzeta@gmail.com>
2011-05-29 12:00:38 +0100

    flaming new SURBL spam checker plus unittest

---
+++ spam/SurblChecker.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+from urlparse import urlparse
+from socket import gethostbyname
+from DomainInexistentException import DomainInexistentException
+
+class SurblChecker(object):
+    """spam checker using surbl"""
+    IS_SPAM = 1
+    IS_NOT_SPAM = 2
+
+    def __init__(self, two_level_file, three_level_file):
+        super(SurblChecker, self).__init__()
+        self._load_datafiles(two_level_file, three_level_file)
+    
+    def _load_datafiles(self, two, three):
+        """docstring for load_datafiles"""
+        self._two_list = list(two.readlines())
+        self._three_list = list(three.readlines())
+
+    def _binary_search(self, a, x, lo=0, hi=None):
+        if hi is None:
+            hi = len(a)
+        while lo < hi:
+            mid = (lo+hi)//2
+            midval = a[mid]
+            if midval < x:
+                lo = mid+1
+            elif midval > x: 
+                hi = mid
+            else:
+                return mid
+        return -1
+
+    def _query_surbl(self, name):
+        try:
+            return gethostbyname(name + ".multi.surbl.org")
+        except Exception:
+            return None
+
+    def _extract_registered_name(self, domain):
+        components = domain.split(".")
+        name_to_check = None
+        if len(components) > 2:
+            pos = self._binary_search(self._three_list, components[-3] + "." + components[-2] + "." + components[-1] + "\n")
+            if pos != -1 and len(components) > 3:
+                name_to_check = components[-4] + "." + components[-3] + "." + components[-2] + "." + components[-1]
+        if not name_to_check:
+            pos = self._binary_search(self._two_list, components[-2] + "." + components[-1] + "\n")
+            if pos != -1 and len(components) > 2:
+                name_to_check = components[-3] + "." + components[-2] + "." + components[-1]
+            else:
+                name_to_check = components[-2] + "." + components[-1]
+        return name_to_check
+
+    def _decode_surbl(self, surbl_result):
+        """decode surbl ip codes"""
+        if surbl_result:
+            return self.IS_SPAM
+        else:
+            return self.IS_NOT_SPAM
+
+    def check_url(self, url):
+        """check an url"""
+        domain = urlparse(url).netloc
+        return self.check_domain(domain)
+
+    def check_domain(self, domain):
+        """check a domain"""
+        domain = domain[domain.find('@')+1:] # remove user info
+        if domain.count(":") > 0:
+            domain = domain[:domain.find(':')] # remove port info
+        
+        name_to_check = self._extract_registered_name(domain)
+        surbl_result = self._query_surbl(name_to_check)
+        return self._decode_surbl(surbl_result)
+
---
+++ tests/SurblTest.py
@@ -0,0 +1,31 @@
+import unittest
+import sys
+import StringIO
+
+sys.path.append("..")
+from spam.SurblChecker import SurblChecker
+
+class MockSurblChecker(SurblChecker):
+    def _query_surbl(self, zone):
+        if zone.startswith("doevil.com"):
+            return "127.0.0.2"
+        return None
+
+class TestSurblChecker(unittest.TestCase):
+    def setUp(self):
+        two = StringIO.StringIO("a.com\nc.nz\ns.it\n")
+        three = StringIO.StringIO("qy.sa.com\nu.cc.nz\nii.sss.it\n")
+        self.checker = MockSurblChecker(two, three)
+
+    def test_spammer(self):
+        result = self.checker.check_url("http://doevil.com/")
+        self.assertEqual(result, MockSurblChecker.IS_SPAM)
+
+    def test_innocent(self):
+        result = self.checker.check_url("http://dogood.com/")
+        self.assertEqual(result, MockSurblChecker.IS_NOT_SPAM)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
