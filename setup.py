748fcc6049b46fda5252193b94ebef04bf2dd542
Federico Marani <flagzeta@gmail.com>
2012-05-26 01:26:35 +0100

    other test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='spam-blocklists',
         author="Federico Marani",
         author_email="flagzeta@gmail.com",
         packages=['spam'],
-        package_data={'spam': ['spam/two-level-tlds', 'spam/three-level-tlds']},
+        package_data={'spam': ['two-level-tlds', 'three-level-tlds']},
         requires=[],    
         classifiers = [
             "Intended Audience :: Developers",
